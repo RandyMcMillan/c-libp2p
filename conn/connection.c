9f30d543f21b7b282d215970cf58264f163b9789
randymcmillan <randymcmillan@protonmail.com>
2026-09-03 23:26:48 -0400

    conn(connection): initialize Connection struct fields and close socket on free
    - Zero-initialize Connection via memset
    - Set transport_dialer, address, socket_fd=-1 in constructor
    - Actually close socket_fd in free instead of leaving it commented

--- conn/connection.c
+++ conn/connection.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "libp2p/conn/connection.h"
 
 struct Connection* libp2p_conn_connection_new(struct TransportDialer* transport_dialer, struct MultiAddress* multiaddress) {
 	struct Connection* out = NULL;
 
-	if (transport_dialer != NULL) {
+	if (transport_dialer != NULL && multiaddress != NULL) {
 		out = (struct Connection*)malloc(sizeof(struct Connection));
 		if (out != NULL) {
-			//TODO implement this
+			memset(out, 0, sizeof(struct Connection));
+			out->transport_dialer = transport_dialer;
+			out->address = multiaddress;
+			out->socket_fd = -1;
 		}
 	}
 	return out;
@@ -16,7 +20,10 @@ struct Connection* libp2p_conn_connection_new(struct TransportDialer* transport_
 
 void libp2p_conn_connection_free(struct Connection* connection) {
 	if (connection != NULL) {
-		//close(connection->socket_handle);
+		if (connection->socket_fd >= 0) {
+			close(connection->socket_fd);
+			connection->socket_fd = -1;
+		}
 		free(connection);
 	}
 }
